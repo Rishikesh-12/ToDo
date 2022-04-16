7007dfa7c777ec07b741fe0d333304acb3f64ca3
Rishikesh Mishra <contactrishikeshhere@gmail.com>
2022-04-17 00:03:18 +0530

    created clock.js
    JavaScript file containing update and display date-time functionalities.

---
+++ clock.js
@@ -0,0 +1,23 @@
+
+var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+var timerID = setInterval(updateTime, 1000);
+
+
+updateTime();
+
+function updateTime() {
+    var cd = new Date();
+    time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    date = zeroPadding(cd.getDate(), 2) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getFullYear(), 4) + ' ' + week[cd.getDay()];
+    document.getElementById("date").innerHTML = date;
+    document.getElementById("time").innerHTML = time;
+};
+
+
+function zeroPadding(num, digit) {
+    var zero = '';
+    for(var i = 0; i < digit; i++) {
+        zero += '0';
+    }
+    return (zero + num).slice(-digit);
+}
